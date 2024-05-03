8000be17faf6aed84813f8cac359d52fd173cf2f
AlexPapadakis <papadakis.lx@gmail.com>
2024-05-03 18:19:48 +0300

    completed profile set up logic and auth

--- my-react-app/src/components/CompleteSignUpForm.jsx
+++ my-react-app/src/components/CompleteSignUpForm.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { KJUR } from 'jsrsasign';
 
-function CompleteSignUpForm() {
+function CompleteSignUpForm({ onCompleteSignUp}) {
     const [errors, setErrors] = useState({});
 
     const handleSubmit = async (e) => {
